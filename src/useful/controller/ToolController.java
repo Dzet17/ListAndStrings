dc7df19de3bb144d005e37cece8c406fa1431bf8
Dylan Zettler <dylanzettler@gmail.com>
2017-10-03 09:03:40 -0600

    Runner and controller now work, created array list for donut, can now properly run

---
+++ src/useful/controller/ToolController.java
@@ -0,0 +1,23 @@
+package useful.controller;
+
+import java.util.List;
+import java.util.ArrayList;
+import useful.model.Donut;
+
+public class ToolController 
+{
+private List<Donut> donutList;
+
+public ToolController()
+{
+	donutList = new ArrayList<Donut>();
+}
+	
+	public void start() 
+	{
+		Donut temp = new Donut();
+		
+		donutList.add(temp);
+	}
+
+}
---
+++ src/useful/controller/ToolRunner.java
@@ -0,0 +1,13 @@
+package useful.controller;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class ToolRunner 
+{
+	public static void main (String [] args)
+	{
+		ToolController myApp = new ToolController();
+		myApp.start();
+	}
+}
