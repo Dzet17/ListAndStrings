3d8bc22eb7307ed8bb2c5d9435a5fd4f4eb428a0
Dylan Zettler <dylanzettler@gmail.com>
2017-10-03 09:56:09 -0600

    Created Donuts and Donut List

--- src/useful/view/PopupDisplay.java
+++ src/useful/view/PopupDisplay.java
@@ -2,13 +2,27 @@ package useful.view;
 
 import javax.swing.JOptionPane;
 
+/**
+ * The popup display class for CTEC AM Session 2017.
+ * @author Dylan Zettler
+ *
+ */
 public class PopupDisplay 
 {
+	/**
+	 * Displays the supplied message in a popup window
+	 * @param message The message that is displayed
+	 */
 	public void displayText(String message)
 	{
 		JOptionPane.showMessageDialog(null, message);
 	}
 	
+	/**
+	 * Displays the supplied question in a popup and returns the answer as a String
+	 * @param fromQuestion The question to ask the user
+	 * @return The String answer
+	 */
 	public String getResponse(String fromQuestion)
 	{
 		String response = "";
